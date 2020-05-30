a5df87e7962ede71fb78422ae6058e26671519dd
EC2 Default User <ec2-user@ip-172-31-22-18.us-east-2.compute.internal>
2020-05-30 09:33:05 +0000

    add message

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -16,24 +16,30 @@ class RostersController < ApplicationController
      end
 
      def new
-          # @roster = Roster.new
+          @roster = Roster.new
           @categories = Category.all
      end
 
      def create
-          roster = Roster.create(  #image: params["rosters"]["image"],
-                                   last_name: params[:rosters][:last_name],
-                                   first_name: params[:rosters][:first_name],
-                                   last_furigana: params[:rosters][:last_furigana],
-                                   first_furigana: params[:rosters][:first_furigana],
-                                   gender: params[:rosters][:gender],
-                                   birthday: params[:rosters][:birthday],
-                                   category_id: params[:rosters][:category_id],
-                                   email: params[:rosters][:email],
-                                   user_id: current_user.id)
-          roster.age = (Date.today.strftime("%Y%m%d").to_i - roster.birthday.strftime("%Y%m%d").to_i)/10000
-          roster.save
-          redirect_to "/rosters"
+          @roster = Roster.create(#image: params["rosters"]["image"],
+                                  last_name: params[:rosters][:last_name],
+                                  first_name: params[:rosters][:first_name],
+                                  last_furigana: params[:rosters][:last_furigana],
+                                  first_furigana: params[:rosters][:first_furigana],
+                                  gender: params[:rosters][:gender],
+                                  birthday: params[:rosters][:birthday],
+                                  age: (Date.today.strftime("%Y%m%d").to_i - params[:rosters][:birthday].strftime("%Y%m%d").to_i)/10000,
+                                  category_id: params[:rosters][:category_id],
+                                  email: params[:rosters][:email],
+                                  user_id: current_user.id)
+          # @roster.age = (Date.today.strftime("%Y%m%d").to_i - @roster.birthday.strftime("%Y%m%d").to_i)/10000
+          if @roster.save
+               flash[:notice] = "名簿を作成しました"
+               redirect_to "/rosters"
+          else
+               @categories = Category.all
+               render action: :new
+          end
      end
 
      def edit
@@ -42,26 +48,33 @@ class RostersController < ApplicationController
      end
 
      def update
-          roster = Roster.find(params[:id])
-          roster.update( # roster.image = params["rosters"]["image"],
+          @roster = Roster.find(params[:id])
+          @roster.update(#roster.image = params["rosters"]["image"],
                          last_name: params[:rosters][:last_name],
                          first_name: params[:rosters][:first_name],
                          last_furigana: params[:rosters][:last_furigana],
                          first_furigana: params[:rosters][:first_furigana],
                          gender: params[:rosters][:gender],
                          birthday: params[:rosters][:birthday],
+                         age: (Date.today.strftime("%Y%m%d").to_i - params[:rosters][:birthday].strftime("%Y%m%d").to_i)/10000,
                          category_id: params[:rosters][:category_id],
                          email: params[:rosters][:email],
                          attendance: params[:rosters][:attendance],
                          remarks: params[:rosters][:remarks])
-          roster.age = (Date.today.strftime("%Y%m%d").to_i - roster.birthday.strftime("%Y%m%d").to_i)/10000
-          roster.save
-          redirect_to "/rosters/" + params[:id].to_s + "/show"
+          # @roster.age = (Date.today.strftime("%Y%m%d").to_i - @roster.birthday.strftime("%Y%m%d").to_i)/10000
+          if @roster.save
+               flash[:notice] = "名簿を編集しました"
+               redirect_to "/rosters/" + params[:id].to_s + "/show"
+          else
+               @categories = Category.all
+               render action: :edit
+          end
      end
 
      def destroy
-          roster = Roster.find(params[:id])
-          roster.destroy
+          @roster = Roster.find(params[:id])
+          @roster.destroy
+          flash[:notice] = "名簿を削除しました"
           redirect_to "/rosters"
      end
 end
--- app/models/roster.rb
+++ app/models/roster.rb
@@ -2,7 +2,7 @@ class Roster < ApplicationRecord
     belongs_to :category
     belongs_to :user
     #画像投稿
-    mount_uploader :image, ImageUploader
+    # mount_uploader :image, ImageUploader
     #バリデーション
-    validates :first_name,:last_name,:first_furigana,:last_furigana,:gender,:birthday,:email, presence: true
+    validates :first_name,:last_name,:first_furigana,:last_furigana,:gender,:birthday,:email, {presence: true}
 end
